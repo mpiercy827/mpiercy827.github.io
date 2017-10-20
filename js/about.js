70669565d231bd4a72e02bdd1afd9a3d1de0494e
mpiercy827 <mpiercy@gmail.com>
2017-10-20 06:12:18 -0700

    add more to about section

--- js/about.js
+++ js/about.js
@@ -3,13 +3,17 @@ var facts = [
   "My eyes are two different colors",
   "My favorite video game is Donkey Kong Country",
   "I have a 4.5-pound, 3-legged teacup poodle named Mighty",
+  "My favorite musical piece is Steve Reich's <a target='_blank' href='https://www.youtube.com/watch?v=plL2VDAoThU'>Electric Counterpoint</a>",
   "I've only ever lived in California: SoCal before college, Bay Area since",
   "I like to play Smash 4 and have competed in a tournament (I lost)",
   "After college, I spent a few months in India teaching science and filmmaking",
   "I don't watch much TV, but I like Game of Thrones, Dexter, and Spongebob",
   "My first dream career was to deliver pizza",
   "I once did Parkour for a few months",
-  "I read web comics daily. My favorites are <a target='_blank' href='http://www.smbc-comics.com/?id=3457'>SMBC</a> and <a target='_blank' href='http://theoatmeal.com/comics/universe_cat'>The Oatmeal</a>"
+  "I started programming in middle school to make a website for my video game flash movies.",
+  "I read web comics daily. My favorites are <a target='_blank' href='http://www.smbc-comics.com/?id=3457'>SMBC</a> and <a target='_blank' href='http://theoatmeal.com/comics/universe_cat'>The Oatmeal</a>",
+  "My favorite musical artsts are Common and Say Anything.",
+  "I can (kinda) read, write and speak German, and am trying to pick up Spanish"
 ];
 
 var index = Math.floor(Math.random() * facts.length);
